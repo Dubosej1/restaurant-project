2a168488a17ace1afdf937961b9dcae27304a3ac
Johnaven <johnavenfreelance@gmail.com>
2022-03-19 14:12:56 -0400

    Add functionality to change environments in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,62 +4,68 @@ const HtmlWebpackPlugin = require(`html-webpack-plugin`);
 const PATH_SOURCE = path.join(__dirname, "./src");
 const PATH_DIST = path.join(__dirname, "./dist");
 
-module.exports = {
-    mode: `development`,
-    entry: [path.join(PATH_SOURCE, "./index.js")],
-    // entry:   {
-    //     index: `./src/index.js`,
-    // },
-    devtool: `inline-source-map`,
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: `Development`,
-            template: path.join(PATH_SOURCE, `./index.html`),
-        }),
-    ],
-    output: {
-        // filename: `[name].bundle.js`,
-        // path: path.resolve(__dirname, `dist`),
-        path: PATH_DIST,
-        filename: "js/[name].[contenthash].js",
-        publicPath: "/",
-    },
+module.exports = (env) => {
+    const environment = env.environment;
+    const isProduction = environment === `production`;
+    const isDevelopment = environment === `development`;
 
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [`style-loader`, `css-loader`, {
-                    loader: `sass-loader`,
-                    options: {
-                        implementation: require(`sass`),
-                    },
-                },],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: `asset/resource`,
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: `asset/resource`,
-            },
-            {
-                test:/\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: `babel-loader`,
-                    options: {
-                        presets: [
-                            ["@babel/preset-env", {
-                                debug: true,
-                                useBuiltIns: "usage",
-                                corejs: 3,
-                            }],
-                        ],
-                    },
-                }
-            }
+    return {
+        mode: environment,
+        entry: [path.join(PATH_SOURCE, "./index.js")],
+        // entry:   {
+        //     index: `./src/index.js`,
+        // },
+        devtool: `inline-source-map`,
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: `Development`,
+                template: path.join(PATH_SOURCE, `./index.html`),
+            }),
         ],
-    },
+        output: {
+            // filename: `[name].bundle.js`,
+            // path: path.resolve(__dirname, `dist`),
+            path: PATH_DIST,
+            filename: "js/[name].[contenthash].js",
+            publicPath: "/",
+        },
+    
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [`style-loader`, `css-loader`, {
+                        loader: `sass-loader`,
+                        options: {
+                            implementation: require(`sass`),
+                        },
+                    },],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: `asset/resource`,
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: `asset/resource`,
+                },
+                {
+                    test:/\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: `babel-loader`,
+                        options: {
+                            presets: [
+                                ["@babel/preset-env", {
+                                    debug: true,
+                                    useBuiltIns: "usage",
+                                    corejs: 3,
+                                }],
+                            ],
+                        },
+                    }
+                }
+            ],
+        },
+    };
 };
