2c395b79a14e034e943bed54605bbf93521c2d86
Johnaven <johnavenfreelance@gmail.com>
2022-03-20 17:30:05 -0400

    Initial base state of the project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import "./style.css";
-import "./sass/style.scss";
+import './sass/style.scss';
 
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function Root() {
-    return <h1>Hello, world.</h1>;
+  return <h1>Base State</h1>;
 }
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById('root'));
