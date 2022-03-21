ab23f4a62537cd64c77e175b113efd2c25dfdabc
Johnaven <johnavenfreelance@gmail.com>
2022-03-21 16:33:52 -0400

    Save work before adding prop-types package

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+// class NavButton extends React.Component {
+//   render() {
+//     return (
+
+//     );
+//   }
+// }
+
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const nav = this.props.isMobile ? (
+      <MobileNavButton />
+    ) : (
+      <Nav mobile={false} setPage={this.props.setPage} />
+    );
     return (
       <header className="header">
         <img
@@ -9,15 +26,89 @@ class Header extends React.Component {
           className="header__logo"
           alt="Site Logo"
         />
-        <nav className="nav">
-          <button className="btn btn__nav btn__home">Home</button>
-          <button className="btn btn__nav btn__menu">Menu</button>
-          <button className="btn btn__nav btn__location">Location</button>
-          <button className="btn btn__nav btn__contact">Contact</button>
-        </nav>
+        <React.Fragment>{nav}</React.Fragment>
       </header>
     );
   }
 }
+
+Header.propTypes = {
+  isMobile: PropTypes.bool,
+};
+
+class MobileNavButton extends React.Component {
+  render() {
+    <></>;
+  }
+}
+
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const navClassName = this.props.mobile ? `mobileNav` : `nav`;
+    return (
+      <nav className="nav">
+        <button
+          className={`btn btn__${navClassName} btn__home`}
+          onClick={this.props.setPage}
+          page="home"
+        >
+          Home
+        </button>
+        <button
+          className={`btn btn__${navClassName} btn__about`}
+          onClick={this.props.setPage}
+          page="about"
+        >
+          About
+        </button>
+        <button
+          className={`btn btn__${navClassName} btn__menu`}
+          onClick={this.props.setPage}
+          page="menu"
+        >
+          Menu
+        </button>
+        <button
+          className={`btn btn__${navClassName} btn__location`}
+          onClick={this.props.setPage}
+          page="location"
+        >
+          Location
+        </button>
+        <button
+          className={`btn btn__${navClassName} btn__contact`}
+          onClick={this.props.setPage}
+          page="contact"
+        >
+          Contact
+        </button>
+      </nav>
+    );
+  }
+}
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header className="header">
+//         <img
+//           src="https://via.placeholder.com/50"
+//           className="header__logo"
+//           alt="Site Logo"
+//         />
+// <nav className="nav">
+//   <button className="btn btn__nav btn__home">Home</button>
+//   <button className="btn btn__nav btn__menu">Menu</button>
+//   <button className="btn btn__nav btn__location">Location</button>
+//   <button className="btn btn__nav btn__contact">Contact</button>
+// </nav>
+//       </header>
+//     );
+//   }
+// }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,69 @@ import './sass/style.scss';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from './components/header.js';
+import SelectHeader from './components/header.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: `home`,
+      isMobile: false,
+    };
+    this.setPage = this.setPage.bind(this);
+  }
+
+  setPage = (event) => {
+    this.setState({
+      currentPage: event.target.page,
+    });
+  };
+
   render() {
-    return <Header />;
+    return (
+      <SelectHeader
+        currentPage={this.state.currentPage}
+        isMobile={this.state.isMobile}
+        setPage={this.setPage}
+      />
+    );
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// this.setPageToHome = this.setPageToHome.bind(this);
+// this.setPageToAbout = this.setPageToAbout.bind(this);
+// this.setPageToMenu = this.setPageToMenu.bind(this);
+// this.setPageToLocation = this.setPageToLocation.bind(this);
+// this.setPageToContact = this.setPageToContact.bind(this);
+
+// setPageToHome = () => {
+//   this.setState({
+//     currentPage: `home`,
+//   });
+// };
+
+// setPageToAbout = () => {
+//   this.setState({
+//     currentPage: `about`,
+//   });
+// };
+
+// setPageToMenu = () => {
+//   this.setState({
+//     currentPage: `menu`,
+//   });
+// };
+
+// setPageToLocation = () => {
+//   this.setState({
+//     currentPage: `location`,
+//   });
+// };
+
+// setPageToContact = () => {
+//   this.setState({
+//     currentPage: `contact`,
+//   });
+// };
