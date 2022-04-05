1b611aeed56f291456c23b9d3125323e4aaa926d
Johnaven <johnavenfreelance@gmail.com>
2022-04-05 17:04:23 -0400

    Add SectionHeadingWithDeco and HeadingDecoration components to headings.js

--- src/components/headings.js
+++ src/components/headings.js
@@ -5,7 +5,26 @@ export function SectionHeading(props) {
   return <h1 className={`${props.comp}__section-heading`}>{props.text}</h1>;
 }
 
+export function SectionHeadingWithDeco(props) {
+  return (
+    <div className={`${props.comp}__section-heading-container`}>
+      <HeadingDecoration />
+      <SectionHeading comp={props.comp} text={props.text} />
+      <HeadingDecoration />
+    </div>
+  );
+}
+
+function HeadingDecoration() {
+  return <span className="heading-decoration"> </span>;
+}
+
 SectionHeading.propTypes = {
+  comp: PropTypes.string,
+  text: PropTypes.string,
+};
+
+SectionHeadingWithDeco.propTypes = {
   comp: PropTypes.string,
   text: PropTypes.string,
 };
