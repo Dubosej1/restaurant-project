85f769ee0eaedf5133de53d05cba1de96a96d66f
Johnaven <johnavenfreelance@gmail.com>
2022-04-06 15:45:53 -0400

    Add WeightSpan and ComboExtraInfo components.  Add dollar signs to strings in combos.prize arrays.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const divider = `----------`;
+
 export const menu = [
   {
     name: `kids`,
@@ -40,6 +45,99 @@ export const highlights = [
     menuItems: getMenuItems([`kids`]),
   },
 ];
+
+export const combos = [
+  {
+    name: `Monday`,
+    desc: [`Crispy Chicken`],
+    price: [`$5.99`],
+  },
+  {
+    name: `Tuesday`,
+    desc: [`Classic`],
+    price: [`$6.35`, `$7.35`],
+  },
+  {
+    name: `Wednesday`,
+    desc: [`Cheeseburger`],
+    price: [`$6.75`, `$7.15`],
+  },
+  {
+    name: `Thursday`,
+    desc: [`Bacon Jelly`],
+    price: [`$6.75`, `$7.15`],
+  },
+  {
+    name: `Friday`,
+    desc: [`Cheddar`],
+    price: [`$5.99`, `$7.15`],
+  },
+  {
+    name: `Saturday`,
+    desc: [`Barbejack`],
+    price: [`$7.15`, `$7.50`],
+  },
+  {
+    name: `Sunday`,
+    desc: [`Italian`],
+    price: [`$6.15`, `$7.15`],
+  },
+  {
+    name: `Salmon Combo`,
+    desc: [null],
+    price: [`$6.95`, `$7.75`],
+  },
+  {
+    name: `Veggie Combo`,
+    desc: [null],
+    price: [`$6.15`, `$6.70`],
+  },
+  {
+    name: `Green Combo (vegetable)`,
+    desc: [null],
+    price: [`$6.15`, `$6.70`],
+  },
+  {
+    name: `Traditional Dog Combo`,
+    desc: [`Fries & Drink`],
+    price: [`$4.77`],
+  },
+  {
+    name: `Include In Your Combo`,
+    desc: [
+      `Chocolate Mini Shake ${divider} $2.90`,
+      `3 Mini Churros or Ice Cream ${divider} $1.50`,
+      `Combo Coffee ${divider} $1.19`,
+      `Juice (instead of soda) ${divider} $2.17 `,
+    ],
+    price: [`$3.99`],
+  },
+];
+
+export function WeightSpan(props) {
+  return <span className={`${props.comp}__weight`}>(1/4 lb) / (1/3 lb)</span>;
+}
+
+WeightSpan.propTypes = {
+  comp: PropTypes.string,
+};
+
+export function ComboExtraInfo() {
+  const item1 = `* All combos can be replaced by burger veggie without additional cost`;
+  const item2 = `* Promotion not cumulative with other promotions`;
+  const item3 = `Hours From 11:30 am to 4pm`;
+
+  return (
+    <div className="combo-item__extra-info-container">
+      <span>{item1}</span>
+      <br></br>
+      <span>{item2}</span>
+      <br></br>
+      <span>{item3}</span>
+      <br></br>
+    </div>
+  );
+}
 
 function getCategory(name) {
   let [category] = categories.filter((category) => category.name === name);
