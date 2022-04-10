a04f272298c5eb7df9f67d7479f24b35e96fcc9f
Johnaven <johnavenfreelance@gmail.com>
2022-04-10 16:56:27 -0400

    Add location info to footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import FacebookLogo from '../assets/facebook-logo.png';
 // import InstagramLogo from '../assets/instagram.png';
+import MapMarker from '../assets/location-pin.png';
 
 export default function Footer(props) {
   return (
     <div className="footer">
       <SocialMediaContainer />
-      {/* <LocationContainer /> */}
+      <LocationContainer />
     </div>
   );
 }
@@ -28,6 +29,73 @@ function SocialMediaLink(props) {
         <div
           className={`social-media-icon social-media-icon__${props.name}`}
         ></div>
+      </a>
+    </div>
+  );
+}
+
+function LocationContainer() {
+  return (
+    <div className="footer__location">
+      <div className="footer__location-info-container">
+        <StoreName div="footer" />
+        <PhoneNumber div="footer" />
+        <Address div="footer" />
+        <MapLink />
+      </div>
+    </div>
+  );
+}
+
+function StoreName(props) {
+  return (
+    <div className={`location-info__store-name--${props.div}`}>
+      <span>New Haven Store</span>
+    </div>
+  );
+}
+
+StoreName.propTypes = {
+  div: PropTypes.string,
+};
+
+function PhoneNumber(props) {
+  return (
+    <div className={`location-info__phone-number--${props.div}`}>
+      <span>(203) 555-7536</span>
+    </div>
+  );
+}
+
+PhoneNumber.propTypes = {
+  div: PropTypes.string,
+};
+
+function Address(props) {
+  return (
+    <div className={`location-info__address--${props.div}`}>
+      <span>1455 Whalley Ave</span>
+      <span>New Haven, CT 06511</span>
+    </div>
+  );
+}
+
+Address.propTypes = {
+  div: PropTypes.string,
+};
+
+function MapLink() {
+  const mapURL = `https://www.google.com/maps/search/?api=1&query=1455+Whalley+Ave.+New+Haven%2C+CT+06511`;
+
+  return (
+    <div className="location-info__map-link-container">
+      <img
+        className="location-info__map-link-marker"
+        src={MapMarker}
+        alt="Map Marker"
+      ></img>
+      <a className="location-info__map-link" href={mapURL}>
+        OPEN MAP
       </a>
     </div>
   );
