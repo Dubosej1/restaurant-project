651d007b6804b17b96477b4b500f50695e8619af
Johnaven <johnavenfreelance@gmail.com>
2022-04-11 17:38:02 -0400

    Add LocationPageBody component.  Add  SectionHeadingWithDeco component to LocationPageBody component.

--- src/components/location.js
+++ src/components/location.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Footer from './footer.js';
+import { SectionHeadingWithDeco } from './headings.js';
 
 export default function Location() {
   return (
     <div>
-      <h1>Location Works!</h1>
+      <LocationPageBody />
       <Footer />
+    </div>
+  );
+}
+
+function LocationPageBody() {
+  const sectionHeadingElem = <StoreName div="location" />;
+
+  return (
+    <div className="location-main">
+      <SectionHeadingWithDeco comp="location-main" text={sectionHeadingElem} />
+    </div>
+  );
+}
+
+function StoreName(props) {
+  return (
+    <div className={`location-info__store-name--${props.div}`}>
+      <span>New Haven Store</span>
     </div>
   );
 }
