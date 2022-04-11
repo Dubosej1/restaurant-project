c95b03a1c76c698160e438df5e478c0c88d6a186
Johnaven <johnavenfreelance@gmail.com>
2022-04-11 18:43:15 -0400

    Create fullmenu.js component.

---
+++ src/components/fullmenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Footer from './footer.js';
+
+export default function FullMenu() {
+  return (
+    <div className="full-menu">
+      <h1>Full Menu Page Works</h1>
+      <Footer />
+    </div>
+  );
+}
