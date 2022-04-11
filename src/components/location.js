d2698940473ebf4d6de54db94b0b47e900e169a7
Johnaven <johnavenfreelance@gmail.com>
2022-04-11 17:06:36 -0400

    Create location.js component.

---
+++ src/components/location.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Footer from './footer.js';
+
+export default function Location() {
+  return (
+    <div>
+      <h1>Location Works!</h1>
+      <Footer />
+    </div>
+  );
+}
