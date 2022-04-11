daa041b10c3ca72d139ebdc315b66db48c4084e3
Johnaven <johnavenfreelance@gmail.com>
2022-04-11 18:59:39 -0400

    Add SectionHeading and SectionSubheading components to fullmenu.js

--- src/components/fullmenu.js
+++ src/components/fullmenu.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Footer from './footer.js';
+import { SectionHeading, SectionSubheading } from './headings.js';
 
 export default function FullMenu() {
+  const subheading = `We have the option of executive combos during the week`;
   return (
     <div className="full-menu">
-      <h1>Full Menu Page Works</h1>
+      <SectionHeading comp="full-menu" text="Menu" />
+      <SectionSubheading comp="full-menu" text={subheading} />
       <Footer />
     </div>
   );
