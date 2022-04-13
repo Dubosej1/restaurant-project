f76f03f644edab86361cd7a8e3762236c4ad760b
Johnaven <johnavenfreelance@gmail.com>
2022-04-13 19:19:07 -0400

    Add burger category and menu items to menu.  Add getMenuItemsByRange function.

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,6 +5,90 @@ const divider = `----------`;
 
 export const menu = [
   {
+    name: `cheeseburger`,
+    num: 1,
+    price: [`$4.75`, `$5.55`],
+    desc: `meat and cheese`,
+    category: `burgers`,
+  },
+  {
+    name: `classic`,
+    num: 2,
+    price: [`$5.20`, `$5.99`],
+    desc: `meat, cheese, lettuce, tomato, red onion and house mayo`,
+    category: `burgers`,
+  },
+  {
+    name: `power`,
+    num: 3,
+    price: [`$6.30`, `$6.75`],
+    desc: `meat, cheese, onion rings, fried bacon and wasabi moinese.`,
+    category: `burgers`,
+  },
+  {
+    name: `cheddar`,
+    num: 4,
+    price: [`$5.99`, `$6.77`],
+    desc: `meat, cheddar, caramelized onion on black bread.`,
+    category: `burgers`,
+  },
+  {
+    name: `italian`,
+    num: 5,
+    price: [`$5.40`, `$5.60`],
+    desc: `meat, cheese and homemade tomato sauce`,
+    category: `burgers`,
+  },
+  {
+    name: `veggie`,
+    num: 6,
+    price: [`$6.10`, `$6.50`],
+    desc: `hamburger made with chickpeas and shitake, cheese, arugula and tomato`,
+    category: `burgers`,
+  },
+  {
+    name: `salmon`,
+    num: 7,
+    price: [`$6.80`, `$7.60`],
+    desc: `homemade hamburger, mozzarella cheese, arugula, onion and wasabi moinese`,
+    category: `burgers`,
+  },
+  {
+    name: `bacon`,
+    num: 8,
+    price: [`$6.50`, `$7.00`],
+    desc: `meat, house bacon jam with cheese`,
+    category: `burgers`,
+  },
+  {
+    name: `beard jack`,
+    num: 9,
+    price: [`$6.50`, `$7.10`],
+    desc: `meat, cheese, barbecue sauce with Jack Daniel's Whiskey, Onion Crispy and Wasabi Mayo Accompaniment.`,
+    category: `burgers`,
+  },
+  {
+    name: `Crispy Chicken`,
+    num: 10,
+    price: [`$5.98`],
+    desc: `breaded chicken fillet with coleslaw and wasabi mayo`,
+    category: `burgers`,
+  },
+  {
+    name: `cheese bacon`,
+    num: 11,
+    price: [`$6.30`, `$7.00`],
+    desc: `beef, cheddar, mozzarella, bacon and house sauce`,
+    category: `burgers`,
+  },
+  {
+    name: `green burger`,
+    num: 12,
+    price: [`$6.40`],
+    desc: `Veggie burger of Pea and Beetroot, Lettuce, Tomato and Onion`,
+    category: `burgers`,
+  },
+  {
     name: `kids`,
     num: 17,
     price: [`$6.50`],
@@ -21,6 +105,12 @@ export const menu = [
 ];
 
 export const categories = [
+  {
+    name: `burgers`,
+    icon: null,
+    subHead: `Choose white or black brioche bread.  Burgers 100% natural, without preservatives, hormones or antibiotics.`,
+    sizes: null,
+  },
   {
     name: `fries`,
     icon: null,
@@ -139,7 +229,7 @@ export function ComboExtraInfo() {
   );
 }
 
-function getCategory(name) {
+export function getCategory(name) {
   let [category] = categories.filter((category) => category.name === name);
 
   return category;
@@ -154,6 +244,19 @@ function getMenuItems(arr) {
     returnArr.push(menuItem);
   });
   console.log(returnArr);
+
+  return returnArr;
+}
+
+export function getMenuItemsByRange(arr) {
+  let returnArr = [];
+  let startingMenuIndex = arr[0];
+  let endingMenuIndex = arr[1];
+
+  for (let i = startingMenuIndex; i <= endingMenuIndex; i++) {
+    let [menuItem] = menu.filter((item) => item.num === i);
+    returnArr.push(menuItem);
+  }
 
   return returnArr;
 }
