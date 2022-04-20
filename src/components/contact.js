27e6080eb86ec15103b4cca07166c1c895514a58
Johnaven <johnavenfreelance@gmail.com>
2022-04-20 17:11:20 -0400

    Create contact.js component

---
+++ src/components/contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Footer from './footer.js';
+import MapMarker from '../assets/location-pin.png';
+
+export default function Contact() {
+  return <h1>Contact Page is rendered</h1>;
+}
