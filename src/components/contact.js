2eeabffa9fc4fbc053c491e7841c9fceb3531c02
Johnaven <johnavenfreelance@gmail.com>
2022-04-20 17:54:19 -0400

    Add page heading and form greeting to contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Footer from './footer.js';
+import { SectionHeadingWithDeco, SectionHeading } from './headings.js';
 import MapMarker from '../assets/location-pin.png';
 
 export default function Contact() {
-  return <h1>Contact Page is rendered</h1>;
+  return (
+    <div>
+      <ContactPageBody />
+      <Footer />
+    </div>
+  );
+}
+
+function ContactPageBody() {
+  const sectionHeadingElem = (
+    <SectionHeading comp="contact-main" text="Contact Us" />
+  );
+
+  const greeting = `We want to hear you tell us your suggestions or criticisms`;
+
+  return (
+    <div className="contact-main">
+      <SectionHeadingWithDeco comp="contact-main" text={sectionHeadingElem} />
+      <div className="contact-main__info-container">
+        <div className="contact-main__greeting-container">
+          <span className="contact-main__greeting">{greeting}</span>
+        </div>
+        {/* <ContactForm /> */}
+      </div>
+    </div>
+  );
 }
