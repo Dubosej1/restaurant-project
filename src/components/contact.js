df8fd069578e00b89b2f545a32b884d869dce870
Johnaven <johnavenfreelance@gmail.com>
2022-04-25 18:01:46 -0400

    Create ContactForm component to contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Footer from './footer.js';
 import { SectionHeadingWithDeco, SectionHeading } from './headings.js';
@@ -27,8 +27,67 @@ function ContactPageBody() {
         <div className="contact-main__greeting-container">
           <span className="contact-main__greeting">{greeting}</span>
         </div>
-        {/* <ContactForm /> */}
+        <ContactForm />
       </div>
     </div>
+  );
+}
+
+function ContactForm() {
+  const [name, setName] = useState(``);
+  const [email, setEmail] = useState(``);
+  const [phone, setPhone] = useState(``);
+  const [message, setMessage] = useState(``);
+
+  const handleNameInput = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailInput = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePhoneInput = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const handleMessageInput = (e) => {
+    setMessage(e.target.value);
+  };
+
+  return (
+    <form
+      className="contact-form"
+      action="mailto:johnavenfreelance@gmail.com"
+      method="POST"
+      encType="multipart/form-data"
+      name="ContactForm"
+    >
+      <input
+        className="contact-form__input"
+        placeholder="Your name"
+        value={name}
+        onChange={handleNameInput}
+      />
+      <input
+        className="contact-form__input"
+        placeholder="Your email"
+        value={email}
+        onChange={handleEmailInput}
+      />
+      <input
+        className="contact-form__input"
+        placeholder="Area Code/Phone"
+        value={phone}
+        onChange={handlePhoneInput}
+      />
+      <textarea
+        className="contact-form__input textarea"
+        placeholder="Type your message here!"
+        value={message}
+        onChange={handleMessageInput}
+      />
+      <input className="contact-form__submit-btn" type="submit" value="Send" />
+    </form>
   );
 }
